10b5430cba552f5fa192dee7ea4455e8ff3383b4
alfina.megasiwi <alfina.megasiwi@ui.ac.id>
2022-07-19 16:07:37 +0700

    Exploring types

--- section-4/exploring-type.go
+++ section-4/exploring-type.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+var yy = 42
+
+// declared that variable with identifier "z" is of type string
+// go is a static programming language
+// a variable is declared to hold a value of a certain type
+var zz = "Shaken, not stirred"
+var aa string = `James said, 
+"Shaken,
+
+not stirred"`
+
 func ExploringType() {
+	fmt.Println(yy)
+	fmt.Printf("%T\n", yy)
+	fmt.Println(zz)
+	fmt.Printf("%T\n", zz)
+	fmt.Println(aa)
+	fmt.Printf("%T\n", aa)
+	// zz = 43 we couldn't assign 43
 
 }
